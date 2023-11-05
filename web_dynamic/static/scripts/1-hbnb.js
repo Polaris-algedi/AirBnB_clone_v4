1b86b1a7b16edd4798844afa1c2a04b3c85c6a07
Polaris-algedi <karchalmohammed@gmail.com>
2023-11-05 08:51:54 -0800

    Add a new template 1-hbnb.html (based on 0-hbnb.html) and update it. Replace the route 0-hbnb with 1-hbnb in the file 1-hbnb.py (based on 0-hbnb.py) and add a JavaScript script (static/scripts/1-hbnb.js)

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  let checkedAmenities = {};
+
+  $('input[type="checkbox"]').change(function() {
+    if (this.checked) {
+      checkedAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checkedAmenities[$(this).data('id')];
+    }
+    let lst = Object.values(checkedAmenities);
+    if (lst.length > 0) {
+      $('div.amenities h4').text(Object.values(checkedAmenities).join(', '));
+    } else {
+      $('div.amenities h4').html(' ');
+    }
+  });
+});
